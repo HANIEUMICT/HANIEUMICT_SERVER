c4481c8fbb7d98b6a50a04127ac4862f92690dcf
seongho5356 <kjeng7897@gmail.com>
2025-06-20 16:06:12 +0900

    #1 Chore: initial commit

---
+++ src/main/java/hanieum/conik/ConikApplication.java
@@ -0,0 +1,13 @@
+package hanieum.conik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConikApplication.class, args);
+	}
+
+}
