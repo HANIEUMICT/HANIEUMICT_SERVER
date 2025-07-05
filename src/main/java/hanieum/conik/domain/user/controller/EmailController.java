765abbfc8351d35e15aaf8cf7785d441f7138073
seongho5356 <kjeng7897@gmail.com>
2025-07-05 19:50:41 +0900

    #19 Refactor: 인터페이스와 구현체를 분리한다

--- src/main/java/hanieum/conik/domain/user/controller/EmailController.java
+++ src/main/java/hanieum/conik/domain/user/controller/EmailController.java
@@ -2,11 +2,10 @@ package hanieum.conik.domain.user.controller;
 
 import hanieum.conik.domain.user.dto.request.AuthCodeRequest;
 import hanieum.conik.domain.user.dto.request.CertificateRequest;
-import hanieum.conik.domain.user.service.EmailService;
+import hanieum.conik.domain.user.service.EmailServiceImpl;
 import hanieum.conik.global.apiPayload.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,18 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v1/email")
 public class EmailController {
-    private final EmailService emailService;
+    private final EmailServiceImpl emailServiceImpl;
 
     @Operation(summary = "인증번호 전송 API", description = "사용자에게 인증번호를 전송합니다.")
     @PostMapping()
     public ApiResponse<?> sendEmail(@RequestBody @Valid AuthCodeRequest authCodeRequest) {
-        emailService.sendEmail(authCodeRequest);
+        emailServiceImpl.sendEmail(authCodeRequest);
         return ApiResponse.success("이메일 전송 성공");
     }
 
     @Operation(summary = "인증번호 검증 API", description = "인증번호를 검증합니다.")
     @PostMapping("/certificate")
     public ApiResponse<?> sendEmailWithCertificate(@RequestBody @Valid CertificateRequest certificateRequest) {
-        return ApiResponse.success(emailService.certificateEmail(certificateRequest));
+        return ApiResponse.success(emailServiceImpl.certificateEmail(certificateRequest));
     }
 }
