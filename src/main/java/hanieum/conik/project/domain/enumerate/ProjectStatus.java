338c41aecf7f8646fb9df9cef290eda3b7f66c5c
seongho5356 <kjeng7897@gmail.com>
2025-07-21 00:03:12 +0900

    #25 Refactor: enum 값들에 설명을 추가한다

--- src/main/java/hanieum/conik/project/domain/enumerate/ProjectStatus.java
+++ src/main/java/hanieum/conik/project/domain/enumerate/ProjectStatus.java
@@ -1,7 +1,14 @@
 package hanieum.conik.project.domain.enumerate;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ProjectStatus {
-    PUBLIC,
-    PROTECTED,
-    PRIVATE;
+    PUBLIC("공개"),
+    PROTECTED("보호"),
+    PRIVATE("비공개");
+
+    private final String description;
 }
--- src/main/java/hanieum/conik/project/domain/enumerate/ProjectType.java
+++ src/main/java/hanieum/conik/project/domain/enumerate/ProjectType.java
@@ -1,6 +1,13 @@
 package hanieum.conik.project.domain.enumerate;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ProjectType {
-    PRODUCT_DEVELOPMENT,
-    FINAL_PRODUCT;
+    PRODUCT_DEVELOPMENT("제품 개발"),
+    FINAL_PRODUCT("최종 제품 개발");
+
+    private final String description;
 }
