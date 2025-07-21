554eaf4d43210e799bd181d2dfc133fa1064a730
seongho5356 <kjeng7897@gmail.com>
2025-07-21 19:12:38 +0900

    #25 Feat: 공통으로 사용하는 id를 AbstracEntity로 관리한다

---
+++ src/main/java/hanieum/conik/proposal/domain/entity/AbstractEntity.java
@@ -0,0 +1,16 @@
+package hanieum.conik.proposal.domain.entity;
+
+import hanieum.conik.global.domain.BaseEntity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@MappedSuperclass
+public abstract class AbstractEntity extends BaseEntity {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
