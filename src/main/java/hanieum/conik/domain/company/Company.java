192be724bd839a00813a683716c170e54f053066
Jihyeon An <jh011007@naver.com>
2025-07-22 18:06:16 +0900

    #27 Feat: Company 도메인 코드와 엔티티 코드 병합

--- src/main/java/hanieum/conik/domain/company/Company.java
+++ src/main/java/hanieum/conik/domain/company/Company.java
@@ -1,7 +1,7 @@
 package hanieum.conik.domain.company;
 
-import hanieum.conik.global.domain.AbstractEntity;
 import hanieum.conik.domain.company.enumerate.CompanyStatus;
+import hanieum.conik.global.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,27 +10,43 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Company extends AbstractEntity {
+public class Company extends BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String owner;
 
+    @Column(nullable = false)
     private String ownerEmail;
 
+    @Column(nullable = false)
     private String ownerPhoneNumber;
 
+    @Column(nullable = false)
     private String businessType;
 
+    @Column(nullable = false)
     private String industry;
 
-    private String RegistrationNumber;
+    @Column(nullable = false)
+    private String registrationNumber;   // camelCase
 
+    @Column(nullable = false)
     private String registrationCertificateUrl;
 
+    @Column(nullable = false)
     private String bankAccountUrl;
 
+    @Column(nullable = false)
     private String profileUrl;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private CompanyStatus status;
 }
