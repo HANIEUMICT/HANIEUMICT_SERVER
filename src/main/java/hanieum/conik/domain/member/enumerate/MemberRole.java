f99bab78dffa7b0c271cc9454cb6ee070973834e
Jihyeon An <jh011007@naver.com>
2025-07-23 00:24:32 +0900

    #27 Fix: MemberRole 각 값에 설명 추가

--- src/main/java/hanieum/conik/domain/member/enumerate/MemberRole.java
+++ src/main/java/hanieum/conik/domain/member/enumerate/MemberRole.java
@@ -1,7 +1,14 @@
 package hanieum.conik.domain.member.enumerate;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberRole {
-    OWNER,
-    STAFF,
-    INDIVIDUAL
+    OWNER("기업 대표"),
+    STAFF("기업 근로자"),
+    INDIVIDUAL("소상공인");
+
+    private final String description;
 }
