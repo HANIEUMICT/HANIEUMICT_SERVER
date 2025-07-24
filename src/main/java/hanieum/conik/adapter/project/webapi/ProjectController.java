a663bc6ce5adc70b3a390f33266060af8c621003
seongho5356 <kjeng7897@gmail.com>
2025-07-24 19:42:13 +0900

    #41 Chore: 불필요한 주석을 제거한다

--- src/main/java/hanieum/conik/adapter/project/webapi/ProjectController.java
+++ src/main/java/hanieum/conik/adapter/project/webapi/ProjectController.java
@@ -6,17 +6,13 @@ import hanieum.conik.adapter.project.dto.response.MemberProjectQueryResponse;
 import hanieum.conik.application.project.provided.ProjectDrawingSaver;
 import hanieum.conik.application.project.provided.ProjectFinder;
 import hanieum.conik.application.project.provided.ProjectSaver;
-import hanieum.conik.global.adapter.security.AuthDetails;
 import hanieum.conik.global.adapter.security.AuthSourceType;
 import hanieum.conik.global.adapter.security.AuthorizeUser;
 import hanieum.conik.global.apiPayload.response.ApiResponse;
-import hanieum.conik.global.domain.exception.AuthErrorType;
-import hanieum.conik.global.domain.exception.AuthException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,7 +30,6 @@ public class ProjectController {
     @PostMapping("{memberId}/image")
     @AuthorizeUser(sourceType = AuthSourceType.PATH_VARIABLE, paramName = "memberId")
     public ApiResponse<?> uploadImage(@PathVariable("memberId") Long memberId,
-                                      @AuthenticationPrincipal AuthDetails authDetails,
                                       @RequestBody @Valid ProjectDrawingUploadRequest projectDrawingUploadRequest) {
         projectDrawingSaver.saveDrawingFileTemp(projectDrawingUploadRequest);
         return ApiResponse.success("도면 파일 업로드 성공");
@@ -43,16 +38,14 @@ public class ProjectController {
     @Operation(summary = "초기 프로젝트(공고) 생성 API", description = "초기에 프로젝트(공고) 페이지를 생성합니다.")
     @PostMapping("{memberId}/init")
     @AuthorizeUser(sourceType = AuthSourceType.PATH_VARIABLE, paramName = "memberId")
-    public ApiResponse<Long> initProject(@AuthenticationPrincipal AuthDetails authDetails,
-                                         @PathVariable("memberId") Long memberId) {
+    public ApiResponse<Long> initProject(@PathVariable("memberId") Long memberId) {
         return ApiResponse.success(projectSaver.initiate(memberId));
     }
 
     @Operation(summary = "프로젝트(공고) 수정 및 임시저장 API", description = "임시 저장 시, 발급된 프로젝트(공고)에 대해 정보를 수정합니다.")
     @PostMapping("{projectId}/draft")
     @AuthorizeUser(sourceType = AuthSourceType.PATH_VARIABLE, paramName = "memberId")
-    public ApiResponse<ProjectRegisterRequest> saveProjectTemp(@AuthenticationPrincipal AuthDetails authDetails,
-                                                               @PathVariable("projectId") Long projectId,
+    public ApiResponse<ProjectRegisterRequest> saveProjectTemp(@PathVariable("projectId") Long projectId,
                                                                @RequestBody ProjectRegisterRequest projectRegisterRequest) {
         return ApiResponse.success(projectSaver.saveProjectDraft(projectId, projectRegisterRequest));
     }
@@ -60,8 +53,7 @@ public class ProjectController {
     @Operation(summary = "프로젝트(공고) 저장 API", description = "작성 완료 된 프로젝트(공고)를 최종 저장합니다.")
     @PostMapping("{projectId}/final")
     @AuthorizeUser(sourceType = AuthSourceType.PATH_VARIABLE, paramName = "memberId")
-    public ApiResponse<ProjectRegisterRequest> saveProjectFinal(@AuthenticationPrincipal AuthDetails authDetails,
-                                                                @PathVariable("projectId") Long projectId,
+    public ApiResponse<ProjectRegisterRequest> saveProjectFinal(@PathVariable("projectId") Long projectId,
                                                                 @RequestBody @Valid ProjectRegisterRequest projectRegisterRequest) {
         return ApiResponse.success(projectSaver.saveProjectFinal(projectId, projectRegisterRequest));
     }
@@ -69,8 +61,7 @@ public class ProjectController {
     @Operation(summary = "사용자 프로젝트(공고) 조회 API", description = "사용자의 프로젝트(공고) 목록을 조회합니다.")
     @GetMapping("/{memberId}")
     @AuthorizeUser(sourceType = AuthSourceType.PATH_VARIABLE, paramName = "memberId")
-    public ApiResponse<List<MemberProjectQueryResponse>> getMemberProjects(@AuthenticationPrincipal AuthDetails authDetails,
-                                                                           @RequestParam(value = "status", required = false) String status,
+    public ApiResponse<List<MemberProjectQueryResponse>> getMemberProjects(@RequestParam(value = "status", required = false) String status,
                                                                            @PathVariable("memberId") Long memberId) {
         return ApiResponse.success(projectFinder.getMemberProjects(memberId, status));
     }
--- src/main/java/hanieum/conik/global/adapter/security/AuthorizeUser.java
+++ src/main/java/hanieum/conik/global/adapter/security/AuthorizeUser.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// 1. 어노테이션 확장
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AuthorizeUser {
