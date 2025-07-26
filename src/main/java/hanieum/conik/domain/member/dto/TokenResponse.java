7534e96b3c7cf6138b470cbff04487315e152ec7
Jihyeon An <jh011007@naver.com>
2025-07-26 15:49:40 +0900

    #47 Feat: refreshtoken 갱신을 위한 dto 생성

---
+++ src/main/java/hanieum/conik/domain/member/dto/TokenResponse.java
@@ -0,0 +1,8 @@
+package hanieum.conik.domain.member.dto;
+
+public record TokenResponse(
+        String accessToken,
+        String refreshToken,
+        long accessTokenExpiresIn,   // 남은 유효시간 (초)
+        long refreshTokenExpiresIn   // 남은 유효시간 (초)
+) {}
