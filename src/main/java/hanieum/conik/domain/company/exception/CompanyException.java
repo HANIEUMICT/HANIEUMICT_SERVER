b9501e8678202ee7bf27e882c841d85da8fa58e1
Jihyeon An <jh011007@naver.com>
2025-08-03 14:49:37 +0900

    #49 Feat : 기업 정보에 주소를 추가한다.

--- src/main/java/hanieum/conik/domain/company/exception/CompanyException.java
+++ src/main/java/hanieum/conik/domain/company/exception/CompanyException.java
@@ -4,7 +4,6 @@ import hanieum.conik.global.apiPayload.exception.ErrorType;
 import hanieum.conik.global.apiPayload.exception.GlobalException;
 
 public class CompanyException extends GlobalException {
-
     public CompanyException(ErrorType errorType) {
         super(errorType);
     }
