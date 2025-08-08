016d5319facc004f810e4533f0aad47ee809e072
Jihyeon An <jh011007@naver.com>
2025-08-08 19:21:19 +0900

    #62 Feat : Error 타입 수정

--- src/main/java/hanieum/conik/domain/member/exception/MemberErrorType.java
+++ src/main/java/hanieum/conik/domain/member/exception/MemberErrorType.java
@@ -15,7 +15,7 @@ public enum MemberErrorType implements ErrorType {
     INVALID_EMAIL(HttpStatus.BAD_REQUEST, "유효하지 않은 이메일입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,     "존재하지 않는 사용자입니다."),
     EMAIL_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 이메일입니다."),
-    COMPANY_ID_MISSING(HttpStatus.INTERNAL_SERVER_ERROR, "멤버의 회사 정보가 누락되었습니다."),
+    COMPANY_ID_MISSING(HttpStatus.UNPROCESSABLE_ENTITY, "기업 회원의 companyId가 누락되었습니다."),
     INVALID_VERIFICATION_CODE(HttpStatus.UNAUTHORIZED,   "유효하지 않은 이메일 인증 코드입니다."),
     EXPIRED_VERIFICATION_CODE(HttpStatus.UNAUTHORIZED,   "이메일 인증 코드가 만료되었습니다.")
     ;
