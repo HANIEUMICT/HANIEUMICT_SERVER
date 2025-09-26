6cef4782ea902ff6c1178dd5f1a3d9891ba6594e
Jihyeon An <jh011007@naver.com>
2025-09-26 22:38:47 +0900

    #80 Feat: swagger 설명을 추가한다.

--- src/main/java/hanieum/conik/adapter/favorite/webapi/FavoriteController.java
+++ src/main/java/hanieum/conik/adapter/favorite/webapi/FavoriteController.java
@@ -8,6 +8,7 @@ import hanieum.conik.application.member.provided.MemberFinder;
 import hanieum.conik.domain.favorite.dto.FavoriteRequest;
 import hanieum.conik.global.adapter.security.AuthDetails;
 import hanieum.conik.global.apiPayload.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -27,6 +28,12 @@ public class FavoriteController {
     private final FavoriteFinder favoriteFinder;
     private final MemberFinder memberFinder;
 
+    @Operation(summary = "찜 등록 API", description = """
+            ## 특정 프로젝트를 찜합니다.
+            - 기업 회원이 특정 프로젝트를 찜합니다.
+            - 이미 찜한 프로젝트는 중복으로 찜할 수 없습니다.
+            - 찜 등록 성공 시, 찜 ID를 반환합니다.
+            """)
     @PostMapping("/{projectId}")
     public ApiResponse<Long> registerFavorite(
             @AuthenticationPrincipal AuthDetails authDetails,
@@ -37,6 +44,12 @@ public class FavoriteController {
         return ApiResponse.success(saved);
     }
 
+    @Operation(summary = "찜 목록 조회 API", description = """
+            ## 기업 회원의 찜 목록을 조회합니다.
+            - 기업 회원이 찜한 프로젝트 목록을 조회합니다.
+            - 프로젝트의 상세 정보를 포함하여 반환합니다.
+            - 페이징 처리를 지원합니다.
+            """)
     @GetMapping()
     public ApiResponse<Page<ProjectDetailResponse>> getFavoriteList(
             @AuthenticationPrincipal AuthDetails authDetails,
@@ -46,6 +59,11 @@ public class FavoriteController {
         return ApiResponse.success(favoriteFinder.findFavoriteProjects(companyId, pageable));
     }
 
+    @Operation(summary = "찜 삭제 API", description = """
+            ## 특정 찜을 삭제합니다.
+            - 기업 회원이 찜한 프로젝트를 찜 목록에서 삭제합니다.
+            - 찜 삭제 성공 시, 삭제된 찜 ID를 반환합니다.
+            """)
     @DeleteMapping("/{favoriteId}")
     public ApiResponse<Long> deleteFavorite(
             @AuthenticationPrincipal AuthDetails authDetails,
