fda732badea68e61d2f924b3044deb8bb45d0958
EunjinWoo <esther0904@naver.com>
2026-01-23 17:17:29 +0900

    #118 fix: 잘못된 import 수정

--- src/main/java/hanieum/conik/application/deal/required/DealRepository.java
+++ src/main/java/hanieum/conik/application/deal/required/DealRepository.java
@@ -1,6 +1,5 @@
 package hanieum.conik.application.deal.required;
 
-import feign.Param;
 import hanieum.conik.domain.deal.entity.Deal;
 import hanieum.conik.domain.deal.enumerate.DealStep;
 import org.springframework.data.domain.Page;
@@ -8,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
